31f5f19db777f75d2a43c4f62859e9c05429ae3e
Dmitry <sevenkillshot@gmail.com>
2024-08-23 15:17:29 +0300

    finish menu

---
+++ bf-for-beatmaker/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
+<template>
+  <div class="container">
+    <AppHeader />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: auto;
+  padding: 20px;
+}
+</style>
