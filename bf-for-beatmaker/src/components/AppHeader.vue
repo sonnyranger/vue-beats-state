4adccb052499350868557b21beabdda94b4cccb3
Dmitry <sevenkillshot@gmail.com>
2024-09-05 16:35:52 +0300

    register

--- bf-for-beatmaker/src/components/AppHeader.vue
+++ bf-for-beatmaker/src/components/AppHeader.vue
@@ -1,43 +1,72 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { useUserStore } from '@/stores/user'
+import type { ComputedRef } from 'vue'
+import { getAuth, signOut } from 'firebase/auth'
+import { useRouter } from 'vue-router'
+
+const userStore = useUserStore()
+const router = useRouter()
 
 interface IMenuItem {
   label: string
   icon: string
   path: string
+  show: ComputedRef<boolean>
 }
 
 const items = ref<IMenuItem[]>([
   {
     label: 'Авторизация',
     icon: 'pi pi-user',
-    path: '/auth'
+    path: '/auth',
+    show: computed((): boolean => !userStore.userId)
   },
   {
     label: 'Добавить',
     icon: 'pi pi-plus',
-    path: '/'
+    path: '/',
+    show: computed((): boolean => !!userStore.userId)
   },
   {
     label: 'Список отправленных инструменталов',
     icon: 'pi pi-list',
-    path: '/list'
+    path: '/list',
+    show: computed((): boolean => !!userStore.userId)
   },
   {
     label: 'Статистика',
     icon: 'pi pi-chart-pie',
-    path: '/statistic'
+    path: '/statistic',
+    show: computed((): boolean => !!userStore.userId)
   }
 ])
+
+const signOutMethod = async (): Promise<void> => {
+  await signOut(getAuth())
+  router.push('/auth')
+}
 </script>
 
 <template>
   <app-menubar :model="items" class="menu">
     <template #item="{ item, props }">
-      <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
-        <span :class="item.icon" class="p-menuitem-icon"> </span>
-        <span class="ml-2">{{ item.label }}</span>
-      </router-link>
+      <template v-if="item.show">
+        <router-link :to="item.path" class="flex align-items-center" v-bind="props.action">
+          <span :class="item.icon" class="p-menuitem-icon" />
+          <span class="ml-2">{{ item.label }}</span>
+        </router-link>
+      </template>
+    </template>
+    <template #end>
+      <span
+        v-if="userStore.userId"
+        @click="signOutMethod"
+        class="flex align-items-center menu-exit"
+      >
+        <span class="pi pi-sign-out p-menuitem-icon" />
+        <span class="ml-2">Выход</span>
+      </span>
     </template>
   </app-menubar>
 </template>
--- bf-for-beatmaker/src/main.ts
+++ bf-for-beatmaker/src/main.ts
@@ -8,12 +8,29 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Menubar from 'primevue/menubar'
-
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+import Toast from 'primevue/toast'
+import ToastService from 'primevue/toastservice'
 import App from './App.vue'
 import router from './router'
+import ProgressSpinner from 'primevue/progressspinner'
+
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDe2ElMOwxTxhSm_HPFFgiiy2EDhk2XlOw',
+  authDomain: 'beats-cb93b.firebaseapp.com',
+  projectId: 'beats-cb93b',
+  storageBucket: 'beats-cb93b.appspot.com',
+  messagingSenderId: '942228199965',
+  appId: '1:942228199965:web:2c26657009c79e18a0c4f4'
+}
+
+initializeApp(firebaseConfig)
 
 const app = createApp(App)
-
+app.use(ToastService)
 app.use(createPinia())
 app.use(router)
 app.use(PrimeVue, {
@@ -22,5 +39,9 @@ app.use(PrimeVue, {
   }
 })
 app.component('app-menubar', Menubar)
+app.component('app-button', Button)
+app.component('app-input-text', InputText)
+app.component('app-toast', Toast)
+app.component('app-progress', ProgressSpinner)
 
 app.mount('#app')
